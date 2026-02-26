3247433894c304ef5a7b8d4e084ddb1e0b75a7e6
chetan <narwarechetan091@gmail.com>
2026-02-26 17:22:39 +0530

    feat: implement URL service logic

---
+++ backend/src/modules/url/url.service.ts
@@ -0,0 +1,54 @@
+import {nanoid} from "nanoid";
+import { createUrl,findByShortCode,deleteUrl,incrementClickCount } from "./url.repository.js";
+import { error } from "node:console";
+
+export const generateShortCode = () => {
+    return nanoid(7);
+}
+
+export const createShortUrl = async (
+    longUrl : string,
+    userId? : number,
+    expiryDate?: Date,
+) => {
+   let shortCode = generateShortCode();
+   let existing = await findByShortCode(shortCode);
+   while(existing){
+    shortCode = generateShortCode();
+    existing = await findByShortCode(shortCode);
+   }
+
+   return createUrl(shortCode, longUrl, userId, expiryDate);
+};
+
+export const getLongUrl = async (shortCode: string) => {
+    const url = await findByShortCode(shortCode);
+    if(!url){
+        throw new Error("URL not found");
+    }
+
+    if(url.expiryDate && new Date() > url.expiryDate){
+        throw new Error("URL had expired");
+    }
+
+    await incrementClickCount(url.id);
+    return url.longUrl;
+}
+
+export const removeUrl = async (
+  shortCode: string,
+  userId?: number
+) => {
+  const url = await findByShortCode(shortCode);
+
+  if (!url) {
+    throw new Error("URL not found");
+  }
+
+  // If URL is tied to a user, ensure ownership
+  if (url.userId && url.userId !== userId) {
+    throw new Error("Unauthorized");
+  }
+
+  return deleteUrl(shortCode);
+};
